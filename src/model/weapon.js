f6f11d1257d63b40e706e423b6f144cf9ac2aa9f
Yang Wei <wei495@usc.edu>
2019-01-16 17:41:55 -0800

    123

--- src/model/weapon.js
+++ src/model/weapon.js
@@ -2,3 +2,4 @@ const mongoose = require('mongoose')
 const model = mongoose.model('weapon', {name: String, phy: Number, bld: Number, createAt: Date})
 model.null = {isNull: true}
 module.exports = model
+//12312312312
